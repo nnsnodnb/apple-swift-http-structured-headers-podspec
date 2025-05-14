c35a4d86c4ceb0cdef1eb7033c806817e344a063
nnsnodnb <nnsnodnb@gmail.com>
2025-05-14 19:57:15 +0900

    Update apple/swift-http-structured-headers v1.3.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/apple/swift-http-structured-headers.git",
-            .upToNextMinor(from: "1.2.2")
+            .upToNextMinor(from: "1.3.0")
         ),
     ],
     targets: [
